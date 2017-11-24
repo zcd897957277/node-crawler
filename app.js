e73ddba80fb5b4623878582bec6bf2ee53a29d90
zcd897957277 <897957277@qq.com>
2017-11-24 15:07:09 +0800

    ip代理

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 ﻿var http = require("http");//网络请求
 var fs = require("fs");//文件系统，操作文件
-var cheerio = require("cheerio");//类jquery操作获取的html
+var cheerio = require("cheerio"),//类jquery操作获取的html
+    async=require('async'),//控制多页面的并发量
+    express=require('express');
+
+var app=express();
 
 //url 就是你想要爬的网页
 var url = "http://www.baidu.com";
 
+//需要爬的网址
+function getUrls(){
+    var urls=[]
+        ,baseUrl='http://acm.hdu.edu.cn/statistic.php?pid=';//基础网址
+
+    for(var i=1000;i<1100;i++){//页面中id
+        var tmp=baseUrl+i;
+        urls.push(tmp);
+    }
+
+    return urls;
+}
+
+var url_id=0;
+
+//获取页面内容
 function download ( url,callback ) {
     http.get(url,function(res){
         var data = "";
+        url_id++;
         res.on("data",function(chunk){
             data += chunk;
             //获取html页面
-            fs.writeFile(__dirname + "/downWebsite/1.html",data,function(err){
+            fs.writeFile(__dirname + "/downWebsite/"+url_id+".html",data,function(err){
                 if(err){
                     console.log(err);
                 }
@@ -26,59 +47,62 @@ function download ( url,callback ) {
         }
     })
 }
+
 //获取图片
-download( url,function( data ) {
-    if(data){
-        var $=cheerio.load(data);
+download(url, function (data) {
+    if (data) {
+        var $ = cheerio.load(data);
         // 页面中直接放置在body img标签中图片
-        $("img").each(function(i,elem){
-            var imgSrc=$(this).attr("src");
-            if(imgSrc.indexOf('http:')<0){
-                imgSrc="http:"+imgSrc;
+        $("img").each(function (i, elem) {
+            var imgSrc = $(this).attr("src");
+            if (imgSrc.indexOf('http:') < 0) {
+                imgSrc = "http:" + imgSrc;
                 downPic(imgSrc);
-            }else {
+            } else {
                 downPic(imgSrc);
             }
         });
         // 页面中放置在style 中url上的图片
-        var urlImgs=[];
-        var urlImgData=data;
-        var index=1;
-        while(index>0){
-            urlImgData=urlImgData.slice(urlImgData.indexOf('url('));
-            var endNum=parseInt(urlImgData.indexOf(')'));
-            getHtmlImg(urlImgData,endNum);
-            function getHtmlImg(urlImgData,endNum){
-                var urlImg=urlImgData.slice(4,parseInt(endNum-1));
-                if(urlImg.indexOf('.png')>0 || urlImg.indexOf('.jpg')>0 || urlImg.indexOf('.gif')>0){
+        var urlImgs = [];
+        var urlImgData = data;
+        var index = 1;
+        while (index > 0) {
+            urlImgData = urlImgData.slice(urlImgData.indexOf('url('));
+            var endNum = parseInt(urlImgData.indexOf(')'));
+            getHtmlImg(urlImgData, endNum);
+
+            function getHtmlImg(urlImgData, endNum) {
+                var urlImg = urlImgData.slice(4, parseInt(endNum - 1));
+                if (urlImg.indexOf('.png') > 0 || urlImg.indexOf('.jpg') > 0 || urlImg.indexOf('.gif') > 0) {
                     urlImgs.push(urlImg);
                 }
-                urlImgData=urlImgData.slice(4,urlImgData.indexOf('url('));
-                index=urlImgData.indexOf('url(');
+                urlImgData = urlImgData.slice(4, urlImgData.indexOf('url('));
+                index = urlImgData.indexOf('url(');
             }
         }
-        if(urlImgs.length>0){
-            urlImgs.each(function(i,elem){
+        if (urlImgs.length > 0) {
+            urlImgs.each(function (i, elem) {
                 downPic(elem);
             });
         }
+
         function downPic(imgSrc) {
             var reg = /[^\/\\]+$/g;
             var libName = imgSrc.match(reg).join('');
-            var libFilter = libName.slice(0,libName.indexOf('.'));
-            http.get(imgSrc,function(res){
-                var imgData="";
+            var libFilter = libName.slice(0, libName.indexOf('.'));
+            http.get(imgSrc, function (res) {
+                var imgData = "";
                 res.setEncoding("binary");
-                res.on("data",function(chunk){
+                res.on("data", function (chunk) {
                     imgData += chunk;
                 });
-                res.on("end",function(){
-                    if(!fs.existsSync("./downWebsite/imgs")){
+                res.on("end", function () {
+                    if (!fs.existsSync("./downWebsite/imgs")) {
                         fs.mkdirSync("./downWebsite/imgs");
                     }
-                    var imgPath="/"+libFilter+"."+imgSrc.split(".").pop();
-                    fs.writeFile(__dirname + "/downWebsite/imgs"+imgPath,imgData,"binary",function(err){
-                        if(err){
+                    var imgPath = "/" + libFilter + "." + imgSrc.split(".").pop();
+                    fs.writeFile(__dirname + "/downWebsite/imgs" + imgPath, imgData, "binary", function (err) {
+                        if (err) {
                             console.log(err);
                         }
                     })
@@ -88,21 +112,22 @@ download( url,function( data ) {
     }
 });
 //获取js文件
-download( url,function( data ) {
-    if(data){
-        var $=cheerio.load(data);
+download(url, function (data) {
+    if (data) {
+        var $ = cheerio.load(data);
         var scriptFile = $('script').toArray();
-        scriptFile.forEach(function(val,index){
+        scriptFile.forEach(function (val, index) {
             //js中直接写在html文件中部分不予理会
-            if(val.attribs.src != null) {
-                obtainLibName(val.attribs.src,index);
+            if (val.attribs.src != null) {
+                obtainLibName(val.attribs.src, index);
             }
         });
-        function obtainLibName(jsLink,i){
+
+        function obtainLibName(jsLink, i) {
             var reg = /[^\/\\]+$/g;
             var libName = jsLink.match(reg).join('');
-            var libFilter = libName.slice(0,libName.indexOf('.'));
-            if(!fs.existsSync("./downWebsite/js")){
+            var libFilter = libName.slice(0, libName.indexOf('.'));
+            if (!fs.existsSync("./downWebsite/js")) {
                 fs.mkdirSync("./downWebsite/js");
             }
             http.get(jsLink, function (response) {
@@ -111,8 +136,8 @@ download( url,function( data ) {
                 response.on('data', function (datajs) {    //加载到内存
                     dataJs += datajs;
                 }).on('end', function () {          //加载完
-                    fs.writeFile(__dirname + "/downWebsite/js/"+libFilter+".js",dataJs,"binary",function(err){
-                        if(err){
+                    fs.writeFile(__dirname + "/downWebsite/js/" + libFilter + ".js", dataJs, "binary", function (err) {
+                        if (err) {
                             console.log(err);
                         }
                     })
@@ -122,23 +147,24 @@ download( url,function( data ) {
     }
 });
 //获取css文件
-download( url,function( data ) {
-    if(data){
-        var $=cheerio.load(data);
+download(url, function (data) {
+    if (data) {
+        var $ = cheerio.load(data);
         var linkFile = $('link').toArray();
-        linkFile.forEach(function(val,index){
+        linkFile.forEach(function (val, index) {
             //link中没有href的部分
-            if(val.attribs.href != null) {
-                if((val.attribs.href).indexOf(".css")>0){
-                    obtainLibName(val.attribs.href,index);
+            if (val.attribs.href != null) {
+                if ((val.attribs.href).indexOf(".css") > 0) {
+                    obtainLibName(val.attribs.href, index);
                 }
             }
         });
-        function obtainLibName(jsLink,i){
+
+        function obtainLibName(jsLink, i) {
             var reg = /[^\/\\]+$/g;
             var libName = jsLink.match(reg).join('');
-            var libFilter = libName.slice(0,parseInt(libName.indexOf('.css')+1));
-            if(!fs.existsSync("./downWebsite/css")){
+            var libFilter = libName.slice(0, parseInt(libName.indexOf('.css') + 1));
+            if (!fs.existsSync("./downWebsite/css")) {
                 fs.mkdirSync("./downWebsite/css");
             }
             http.get(jsLink, function (response) {
@@ -147,8 +173,8 @@ download( url,function( data ) {
                 response.on('data', function (datacss) {    //加载到内存
                     dataCss += datacss;
                 }).on('end', function () {          //加载完
-                    fs.writeFile(__dirname + "/downWebsite/css/"+libFilter+".css",dataCss,"binary",function(err){
-                        if(err){
+                    fs.writeFile(__dirname + "/downWebsite/css/" + libFilter + ".css", dataCss, "binary", function (err) {
+                        if (err) {
                             console.log(err);
                         }
                     })
@@ -157,4 +183,10 @@ download( url,function( data ) {
         }
     }
 });
+
+//listen
+app.listen(3000,function(){
+    console.log('app is listening at port 3000.');
+});
+
 
---
+++ ip-proxy/README.md
@@ -0,0 +1,16 @@
+分别尝试用 superagent 以及 request 来做 ip 代理的 HTTP 请求库。
+
+使用方法非常简单，首先通过不同渠道获取一些免费 ip，然后通过相应 api 去模拟请求。
+
+一些免费 ip 库：
+
+http://www.xicidaili.com/nn
+http://www.66ip.cn/
+http://cn-proxy.com/
+http://www.kuaidaili.com/free/
+http://ip.chinaz.com/getip.aspx（验证代理是否可用）
+这里 有一个很好的帖子给了我思路，首先获取免费 ip，然后去请求 http://ip.chinaz.com/getip.aspx ，因为这个网站本身就是通过 ip 来验证归属地的，通过请求的结果就能判断 ip 是否可用，适当设置 timeout 参数。
+
+todo:
+
+proxies 在你访问 http 时用 http 的设置，访问 https 时用 https 的设置。原代码中只考虑了 http 的情况，可参考 https://segmentfault.com/q/1010000009677441
